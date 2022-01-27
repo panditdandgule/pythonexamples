2150af4e1cd11751e9ac7bfe854a3f4fe4fd1ee3
pandit dandgule <panditdandgule777@gmail.com>
2022-01-27 19:09:12 +0530

    checking values.

---
+++ w3resource/excercises/dictionaries/checksamevalues.py
@@ -0,0 +1,7 @@
+dict_values = {'Cierra Vega': 12, 'Alden Cantrell': 12, 'Kierra Gentry': 12, 'Pierre Cox': 12}
+
+for key, value in dict_values.items():
+    if value == 12:
+        print(True)
+    else:
+        print(False)
