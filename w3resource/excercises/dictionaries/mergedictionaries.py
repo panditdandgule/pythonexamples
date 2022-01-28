94b1bdd224eac7214cf754d470bc1a6adfc8a280
pandit dandgule <panditdandgule777@gmail.com>
2022-01-28 17:38:36 +0530

    combining two dictionaries(added one item in dict_str)

--- w3resource/excercises/dictionaries/mergedictionaries.py
+++ w3resource/excercises/dictionaries/mergedictionaries.py
@@ -1,5 +1,5 @@
 dict_num = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50}
-dict_str = {'A': 64, 'B': 65, 'C': 66}
+dict_str = {'A': 64, 'B': 65, 'C': 66,'D':67}
 
 print("Dictionary First: ", dict_num)
 print("Dictionary Second: ", dict_str)
