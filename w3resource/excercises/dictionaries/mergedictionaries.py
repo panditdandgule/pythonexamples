aa02a37125f7565b3ed2f44c5f920f7cae375327
pandit dandgule <panditdandgule777@gmail.com>
2022-01-28 17:35:03 +0530

    combining two dictionaries

---
+++ w3resource/excercises/dictionaries/mergedictionaries.py
@@ -0,0 +1,9 @@
+dict_num = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50}
+dict_str = {'A': 64, 'B': 65, 'C': 66}
+
+print("Dictionary First: ", dict_num)
+print("Dictionary Second: ", dict_str)
+
+new_dict = {**dict_num, **dict_str}
+
+print("Combining Dictionaries: ", new_dict)
