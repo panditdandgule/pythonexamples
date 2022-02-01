2ed297a2b77b81b1825f396fd59f6c4ce0299574
pandit dandgule <panditdandgule777@gmail.com>
2022-02-01 17:01:07 +0530

    fibnocci series.

---
+++ w3resource/excercises/controlflow/fibonacciseries.py
@@ -0,0 +1,18 @@
+a = 0
+b = 1
+
+while b < 50:
+    print(b, end=" ")
+    a, b = b, a + b
+
+'''
+for i in range(1,50):
+    print(a)
+    a,b=b,a+b
+
+while i<50:
+    print(a,end=" ")
+    a,b=b,a+b
+    i+=1
+
+'''
