dbc6372b5cdadd3f4f7267f65f9fe9d8b5a4a6ee
pandit dandgule <panditdandgule777@gmail.com>
2022-02-01 17:35:52 +0530

    infinite loop

---
+++ w3resource/excercises/controlflow/infinte.py
@@ -0,0 +1,9 @@
+while True:
+    user=input("Enter your name: ")
+
+    if user=='pandit':
+        print("Welcome pandit")
+        break
+    else:
+        print("Input is not valid try again..")
+        continue
