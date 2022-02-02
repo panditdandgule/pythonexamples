0bec538167e670396b0f483bb3b789728bd0cba5
pandit dandgule <panditdandgule777@gmail.com>
2022-02-02 18:06:27 +0530

    convert list to strings

--- w3resource/excercises/mapfunction/upperlower.py
+++ w3resource/excercises/mapfunction/upperlower.py
@@ -1,11 +1,13 @@
-chars={'a','b','E','f','a','e','i','o','u','U'}
+chars = {'a', 'b', 'E', 'f', 'a', 'e', 'i', 'o', 'u', 'U'}
+
 
 def change_case(s):
-    return str(s).upper(),str(s).lower()
+    return str(s).upper(), str(s).lower()
 
-print("Original Characters:\n",chars)
 
-result=map(change_case,chars)
+print("Original Characters:\n", chars)
+
+result = map(change_case, chars)
 print("\nAfter converting above characters in uppercase and lowercase")
 
 print(set(result))
