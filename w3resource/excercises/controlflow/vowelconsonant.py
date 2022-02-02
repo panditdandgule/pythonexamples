78cd4d0a9bb41264ad6d1cf93bf4a29fd33602c9
pandit dandgule <panditdandgule777@gmail.com>
2022-02-02 15:41:58 +0530

    check vowel or consonant

---
+++ w3resource/excercises/controlflow/vowelconsonant.py
@@ -0,0 +1,8 @@
+l=input("Enter alphabeat: ")
+
+if l in ('a','e','i','o','u'):
+    print("%s is vowel"%l)
+elif l=='y':
+    print("Somtime its called as vowel")
+else:
+    print("%s its consonant"%l)
