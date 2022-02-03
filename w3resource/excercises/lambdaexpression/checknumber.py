302c5cd3126dc4a12a863782d5b5941b911e4f9b
pandit dandgule <panditdandgule777@gmail.com>
2022-02-03 10:40:32 +0530

    Check digit or string.

---
+++ w3resource/excercises/lambdaexpression/checknumber.py
@@ -0,0 +1,8 @@
+result = lambda x: x.replace('.', '', 1).isdigit()
+
+print(result('1'))
+print(result('one'))
+print(result('132.45'))
+result1 = lambda y: True if y.isdigit() else False
+print(result1('1343'))
+print(result1('onetwo'))
