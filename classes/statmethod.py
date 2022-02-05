1e79144bae88dfb60ef454563cc0d883a44dc595
pandit dandgule <panditdandgule777@gmail.com>
2022-02-05 09:58:14 +0530

    static  methods.

---
+++ classes/statmethod.py
@@ -0,0 +1,13 @@
+class DurgaMath:
+    @staticmethod
+    def add(x,y):
+        print("The sum",x+y)
+
+    @staticmethod
+    def product(x,y):
+        print("The product:",x*y)
+
+d=DurgaMath()
+DurgaMath.add(10,20)
+DurgaMath.product(2,3)
+d.add(20,30)
